fb481711d27c48c5d5a0536b8ae2dcfde7596ca1
Alex <alex.gascon1999@gmail.com>
2023-12-03 18:50:04 +0100

    Best yet

--- spoty_stats.py
+++ spoty_stats.py
@@ -1,36 +1,59 @@
 import os
+import re
 import json
 import pandas as pd
 from datetime import datetime, timedelta
+from collections import Counter
+import matplotlib.pyplot as plt
 
+PATH = r'C:\Users\usuario\OneDrive\Escritorio\VS files\spoty'
+MIN_MS = 20000
+SEPARA_TV = False
+TODAY = datetime.now().strftime("%d%m%Y")
 
 def leer_archivo(archivo):
     try:
-        with open(archivo, 'r', encoding='latin1') as file:
+        with open(archivo, 'r', encoding='utf-8') as file:
             return json.load(file)
     except FileNotFoundError:
         print(f"Error: No se pudo encontrar el archivo {archivo}")
         return []
 
-def calcular_tiempo_reproduccion(archivos):
+def get_time_per_song(archivos):
     tiempo_por_cancion = {}
 
     for archivo in archivos:
         data = leer_archivo(archivo)
 
+        streamings_list = []
+        list_total_hours = []
+        list_total_days = []
+
+
         for registro in data:
             artist_name = registro['artistName']
-            track_name = registro['trackName']
-            track_name = track_name.replace(",", ";")
+            track_name = registro['trackName'].replace(",", ";")
+            # track_name = track_name.replace(",", ";")
             ms_played = registro['msPlayed']
             end_time = datetime.strptime(registro['endTime'], "%Y-%m-%d %H:%M")
+            day, hour = registro['endTime'].split(" ")
+            list_total_hours.append(hour)
+            list_total_days.append(day)
+
 
             key = f"{artist_name} - {track_name}"
+            
+            if SEPARA_TV == False:
+                if "Version" in key:
+                    key = re.sub(r'\([^)]*\)', '', key).rstrip()
+                    track_name = re.sub(r'\([^)]*\)', '', track_name).rstrip()
 
             if key in tiempo_por_cancion:
                 tiempo_por_cancion[key]['ms_played'] += ms_played
                 tiempo_por_cancion[key]['reproducciones'] += 1
                 tiempo_por_cancion[key]['last_played'] = end_time
+                if ms_played < MIN_MS:
+                    tiempo_por_cancion[key]['skips'] += 1
             else:
                 tiempo_por_cancion[key] = {
                     'artist': artist_name,
@@ -38,10 +61,11 @@ def calcular_tiempo_reproduccion(archivos):
                     'ms_played': ms_played,
                     'reproducciones': 1,
                     'first_played': end_time,
-                    'last_played': end_time
-                }
+                    'last_played': end_time,
+                    'skips': 0 if ms_played >= MIN_MS else 1
+    }
 
-    return tiempo_por_cancion
+    return tiempo_por_cancion, list_total_days, list_total_hours
 
 def mostrar_resultados(tiempo_por_cancion):
     # Ordenar los resultados por la duración total de reproducción de mayor a menor
@@ -49,44 +73,44 @@ def mostrar_resultados(tiempo_por_cancion):
 
     # Crear un DataFrame con los resultados
     df = pd.DataFrame(resultados_ordenados, columns=['Cancion', 'Datos'])
-
-    # Crear un nuevo DataFrame para las columnas 'Artista' y 'Título'
     df_artist_title = pd.DataFrame(df['Datos'].tolist(), index=df.index)
-
-    # Concatenar los DataFrames
     df = pd.concat([df, df_artist_title], axis=1)
 
-    # Calcular minutos, duración y formato de la fecha
+
     df['Minutos'] = df['Datos'].apply(lambda x: x['ms_played'] / (1000 * 60))
     df['Duracion'] = df['Datos'].apply(lambda x: str(timedelta(milliseconds=x['ms_played'])).split(".")[0])
     df['Milisegundos'] = df['Datos'].apply(lambda x: x['ms_played'])
     df['Reproducciones'] = df['Datos'].apply(lambda x: x['reproducciones'])
-    df['Primera reproduccion'] = df['Datos'].apply(lambda x: x['first_played'].strftime("%Y-%m-%d %H:%M"))
-    df['Última reproduccion'] = df['Datos'].apply(lambda x: x['last_played'].strftime("%Y-%m-%d %H:%M"))
+    df['First stream'] = df['Datos'].apply(lambda x: x['first_played'].strftime("%Y-%m-%d %H:%M"))
+    df['Last stream'] = df['Datos'].apply(lambda x: x['last_played'].strftime("%Y-%m-%d %H:%M"))
+    df['Skips'] = df['Datos'].apply(lambda x: x['skips'])
+    df['Skip %'] = (df['Datos'].apply(lambda x: x['skips']) / df['Datos'].apply(lambda x: x['reproducciones']))
 
-    # Eliminar filas con menos de 20 segundos 
-    df = df[df['Milisegundos'] > 20000]
 
-    # Eliminar columnas no deseadas
-    # df = df[['artist', 'title', 'Minutos', 'Duración', 'Milisegundos', 'Reproducciones', 'Primera reproducción', 'Última reproducción']]
-    df = df[['artist', 'title', 'Duracion', 'Milisegundos', 'Reproducciones', 'Primera reproduccion', 'Última reproduccion']]
+    df = df[['artist', 'title', 'Duracion', 'Milisegundos', 'Reproducciones', 'First stream', 'Last stream', 'Skips', 'Skip %']]
 
-    # Mostrar el DataFrame
-    print(df)
+    df.to_csv(os.path.join(PATH, f'stats_songs_{TODAY}.csv'), index=False, sep=';', encoding='utf-8')
 
-    # Guardar el DataFrame como un archivo CSV
-    df.to_csv(os.path.join('C:\\Users\\usuario\\OneDrive\\Escritorio\\VS files\\spoty', 'stats.csv'), index=False, sep=';', encoding='latin1')
+
+def get_days_count(days_list):
+    days_count = Counter(days_list)
+    return days_count
+
+def plot_hour(hour_list):
+    datetime_list = [datetime.strptime(hour, "%H:%M") for hour in hour_list]
+    hours = [dt.hour for dt in datetime_list]
+    plt.hist(hours, bins=24, edgecolor='black', alpha=0.7)
+    plt.xlabel('Hora del día')
+    plt.ylabel('Frecuencia')
+    plt.title('Histograma de Horas')
+    plt.xticks(range(24))
+    plt.grid(True)
+    plt.show()
 
 
 if __name__ == "__main__":
-    # Ruta de los archivos
-    directorio = r'C:\Users\usuario\OneDrive\Escritorio\VS files\spoty'
+    files = [os.path.join(PATH, f'StreamingHistory{i}.json') for i in range(3)]
 
-    # Obtener la lista de archivos en la ruta especificada
-    archivos = [os.path.join(directorio, f'StreamingHistory{i}.json') for i in range(3)]
+    tiempo_por_cancion, dias, horas = get_time_per_song(files)
 
-    # Calcular el tiempo total de reproducción y el número de reproducciones por canción
-    tiempo_por_cancion = calcular_tiempo_reproduccion(archivos)
-
-    # Mostrar los resultados y guardar en CSV
     mostrar_resultados(tiempo_por_cancion)
